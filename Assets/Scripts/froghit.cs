24e5efccf0e93d7ac27a2f0299942fb00169dc46
cpizzimenti <courtneyl.pizzimenti@gmail.com>
2021-11-30 09:57:38 -0500

    Initial commit

---
+++ Assets/Scripts/froghit.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class froghit : MonoBehaviour
+{
+
+    public GameObject explosionEffect;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Vector3 particleSpawnPoint = other.transform.position;
+        Instantiate(explosionEffect, particleSpawnPoint, Quaternion.identity);
+        //Destroy() is base func for destroying components and objevts in a scene
+        //you need ot specify that you are refering to a game object
+        //Destroy(this) will destroy the component not the object
+        //Destroy(this.game Object) wil destry th eobject where this compnonet is
+        Destroy(other.gameObject);
+    }
+}
