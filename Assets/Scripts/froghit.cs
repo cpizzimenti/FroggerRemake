e6019e497fd65b3d84df3a94d638ea8083020d8f
cpizzimenti <courtneyl.pizzimenti@gmail.com>
2021-12-05 17:28:27 -0500

    destroy cars and game over

--- Assets/Scripts/froghit.cs
+++ Assets/Scripts/froghit.cs
@@ -1,20 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class froghit : MonoBehaviour
 {
 
-    public GameObject explosionEffect;
-
-    private void OnTriggerEnter(Collider other)
+    public void LoadLevel(string name)//function with string parameter, void is return type.
     {
-        Vector3 particleSpawnPoint = other.transform.position;
-        Instantiate(explosionEffect, particleSpawnPoint, Quaternion.identity);
-        //Destroy() is base func for destroying components and objevts in a scene
-        //you need ot specify that you are refering to a game object
-        //Destroy(this) will destroy the component not the object
-        Destroy(this.gameObject);//wil destry th eobject where this compnonet is
-        //Destroy(other.gameObject);
+        SceneManager.LoadScene(name);
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        LoadLevel("gameover");
+    }
+
+    //private void OnCollisionEnter2D(Collision2D collision)
+  //  {
+    /// 
+         //   Destroy(gameObject);
+        
+   // }
 }
