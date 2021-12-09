b6f3bc83c10b7f54bb0892513847a271e3a0b80b
Maveric <mmuscat@ltu.edu>
2021-12-09 03:33:41 -0500

    Deleted unused files

--- Assets/Scripts/PowerUp.cs
+++ Assets/Scripts/PowerUp.cs
@@ -6,7 +6,6 @@ using UnityEngine.UI;
 public class PowerUp : MonoBehaviour
 {
     public GameObject pickup;
-
     public VehicleBehaviour car;
     public VehicleBehaviour bus;
     public VehicleBehaviour2 car2;
--- Assets/Scripts/PowerUpController.cs
+++ Assets/Scripts/PowerUpController.cs
@@ -14,6 +14,7 @@ public class PowerUpController : MonoBehaviour
     {
         Vector3 powerUpPosition = new Vector3(Random.Range(-10f, 10f), Random.Range(-4.5f, 4.5f), 0f);
         Instantiate(powerUp, powerUpPosition, Quaternion.identity);
+        print("Power up spawned.");
     }
 
     void Start()
@@ -33,6 +34,7 @@ public class PowerUpController : MonoBehaviour
             car2.Speed = car2.Speed/2;
             bus.Speed = bus.Speed/2;
             bus2.Speed = bus2.Speed/2;
+            print("Collide!");
         }
 
     }
